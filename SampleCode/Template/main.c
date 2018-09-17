f96209672d05a2c673a06a55bb296ce9d35de64c
MS20 WSChang0 <WSChang0@nuvoton.com>
2018-09-17 15:05:25 +0800

    Initialization

---
+++ SampleCode/Template/main.c
@@ -0,0 +1,59 @@
+/******************************************************************************
+ * @file     main.c
+ * @version  V1.00
+ * $Revision: 2 $
+ * $Date: 16/10/25 4:34p $
+ * @brief    Software Development Template.
+ * @note
+ * Copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+#include <stdio.h>
+#include "NUC1261.h"
+
+
+void SYS_Init(void)
+{
+    /* Enable IP clock */
+    CLK->APBCLK0 = CLK_APBCLK0_UART0CKEN_Msk;
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock and cyclesPerUs automatically. */
+    SystemCoreClockUpdate();
+
+    /* Set PA multi-function pins for UART0 RXD and TXD */
+    SYS->GPA_MFPL &= ~(SYS_GPA_MFPL_PA2MFP_Msk | SYS_GPA_MFPL_PA2MFP_Msk);
+    SYS->GPA_MFPL |= (SYS_GPA_MFPL_PA3MFP_UART0_RXD | SYS_GPA_MFPL_PA2MFP_UART0_TXD);
+
+
+}
+
+
+int main()
+{
+    int8_t ch;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    SYS_Init();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+    /* Init UART0 to 115200-8n1 for print message */
+    UART_Open(UART0, 115200);
+
+    printf("Simple Demo Code\n\n");
+
+    printf("Please Input Any Key\n\n");
+
+    do
+    {
+        printf("Input: ");
+        ch = getchar();
+        printf("%c\n", ch);
+    }
+    while(1);
+}
+
+/*** (C) COPYRIGHT 2016 Nuvoton Technology Corp. ***/
